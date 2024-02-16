f836924ca23a4349c426eefa1e2d77a543864724
JimmyDeveloperMan <jimmy.moftah@gmail.com>
2024-02-16 17:07:45 +0200

    fix:admin action

--- src/actions/is-admin.ts
+++ src/actions/is-admin.ts
@@ -3,8 +3,6 @@ import mongo_connect from "./mongo-connect";
 
 export default async function isAdmin( email : string ) {
   await mongo_connect();
-  const admin = (await UserInfos.findOne({ email }, { admin: 1 })) || {
-    admin: false,
-  };
-  return { admin };
+  const {admin} = await UserInfos.findOne({ email},{admin:1})
+  return  admin 
 }
