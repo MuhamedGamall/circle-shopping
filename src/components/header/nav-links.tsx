85fb1d8d6f45fd44bb3ed9c1c83f62c2c69aeac4
JimmyDeveloperMan <jimmy.moftah@gmail.com>
2024-02-19 17:15:01 +0200

    fix nav links

--- src/components/header/nav-links.tsx
+++ src/components/header/nav-links.tsx
@@ -8,35 +8,35 @@ import useProfile from "@/hooks/user-profile";
 
 export default function NavLinks() {
   const { data, loading } = useProfile();
+  console.log(data);
+  
   return (
     <nav className="flex gap-2 items-center mx-4">
-      {!loading ? (
-        !data ? (
-          <div className="hidden sm:flex gap-2 items-center">
-            <Link
-              href="/account/log-in"
-              className={
-                "flex gap-1 text-secondMain items-center whitespace-nowrap text-sm font-semibold transition-all hover:opacity-[.6] px-2 sm:border-r border-slate-600"
-              }
-            >
-              Log in <User className={"h-4 w-4 text-secondMain/50"} />
-            </Link>
-            <Link
-              href="/account/create-account"
-              className={
-                "flex gap-1  bg-secondMain py-2 px-3 hover:bg-slate-900 rounded-md items-center whitespace-nowrap text-sm text-white font-semibold transition-all "
-              }
-            >
-              Sign up <LogIn className={"h-4 w-4 "} />
-            </Link>
-          </div>
-        ) : (
-          <div className="sm:block hidden">
-            <AccountMenu {...data} />
-          </div>
-        )
+      {loading ? (
+        <span>loading...</span>
+      ) : !!data ? (
+        <div className="sm:block hidden">
+          <AccountMenu {...data} />
+        </div>
       ) : (
-        "Loading..."
+        <div className="hidden sm:flex gap-2 items-center">
+          <Link
+            href="/account/log-in"
+            className={
+              "flex gap-1 text-secondMain items-center whitespace-nowrap text-sm font-semibold transition-all hover:opacity-[.6] px-2 sm:border-r border-slate-600"
+            }
+          >
+            Log in <User className={"h-4 w-4 text-secondMain/50"} />
+          </Link>
+          <Link
+            href="/account/create-account"
+            className={
+              "flex gap-1  bg-secondMain py-2 px-3 hover:bg-slate-900 rounded-md items-center whitespace-nowrap text-sm text-white font-semibold transition-all "
+            }
+          >
+            Sign up <LogIn className={"h-4 w-4 "} />
+          </Link>
+        </div>
       )}
 
       <div className="">
