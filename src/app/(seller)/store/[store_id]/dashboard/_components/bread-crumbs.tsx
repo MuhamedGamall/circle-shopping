0365fa0bf9548240f1c5143d50c638593199532f
JimmyDeveloperMan <jimmy.moftah@gmail.com>
2024-02-27 22:10:27 +0200

    update bread crumbs

--- src/app/(seller)/store/[store_id]/dashboard/_components/bread-crumbs.tsx
+++ src/app/(seller)/store/[store_id]/dashboard/_components/bread-crumbs.tsx
@@ -1,18 +1,12 @@
 "use client";
 import { cn } from "@/lib/utils";
-import { truncateText } from "@/utils/truncate-text";
 import { LucideLayoutDashboard } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { BsChevronRight } from "react-icons/bs";
-import { useWindowSize } from "react-use";
 
 export default function BreadCrumbs() {
   const pathname = usePathname();
-
-  const { width: innerWidth } = useWindowSize();
-
-  const minWidth = innerWidth <= 640;
 
   const crumbs = pathname.split("/").slice(3);
   const hiddenLink = `http://localhost:3000/${pathname
@@ -23,9 +17,9 @@ export default function BreadCrumbs() {
   return (
     <div
       className={cn(
-        "flex sm:items-center gap-3 mb-5 break-all  overflow-x-auto",
+        " hidden sm:flex sm:items-center gap-3 mb-5 break-all  overflow-x-auto",
         {
-          hidden: crumbs.length === 1,
+          'hidden': crumbs.length === 1,
         }
       )}
     >
@@ -33,23 +27,21 @@ export default function BreadCrumbs() {
         return (
           <>
             {parseFloat(crumb) ? (
-              <span className="cursor-default  text-sm text-slate-600">
-                {minWidth ? truncateText(crumb, 10) : crumb}
+              <span className="cursor-default  font-semibold sm:text-sm text-[11px] text-slate-600">
+                {crumb}
               </span>
             ) : (
               <Link
                 href={`${hiddenLink}${crumbs.slice(0, i + 1).join("/")}`}
                 className={cn(
-                  "capitalize  font-semibold text-sm text-slate-600 hover:underline",
+                  "capitalize  font-semibold sm:text-sm text-[11px] text-slate-600 hover:underline",
                   {
                     " text-[#3866df]": i === crumbs.length - 1,
                   }
                 )}
               >
                 {crumb === "dashboard" ? (
-                  <LucideLayoutDashboard className="h-4 w-4 text-slate-600" />
-                ) : minWidth ? (
-                  truncateText(crumb.split("-").join(" "), 10)
+                  <LucideLayoutDashboard className="sm:h-4 sm:w-4 h-3 w-3 text-slate-600" />
                 ) : (
                   crumb.split("-").join(" ")
                 )}
