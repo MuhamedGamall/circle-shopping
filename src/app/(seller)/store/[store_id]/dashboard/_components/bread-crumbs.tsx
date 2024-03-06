4f93de0962c82a1ac475d8a4206b38e48dda5b0d
JimmyDeveloperMan <jimmy.moftah@gmail.com>
2024-03-06 16:09:33 +0200

    Change visibility of breadcrumb component on mobile screens

--- src/app/(seller)/store/[store_id]/dashboard/_components/bread-crumbs.tsx
+++ src/app/(seller)/store/[store_id]/dashboard/_components/bread-crumbs.tsx
@@ -13,13 +13,14 @@ export default function BreadCrumbs() {
     .split("/")
     .slice(1, 3)
     .join("/")}/`;
+console.log(crumbs.length);
 
   return (
     <div
       className={cn(
         " hidden sm:flex sm:items-center gap-3 mb-5 break-all  overflow-x-auto",
         {
-          'hidden': crumbs.length === 1,
+          'sm:hidden': crumbs.length === 1,
         }
       )}
     >
