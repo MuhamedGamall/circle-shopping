ac45f1a8a0962e26a42104ba1a2ecdf096ee4e39
JimmyDeveloperMan <jimmy.moftah@gmail.com>
2024-04-03 17:24:54 +0200

    update create category form

--- src/app/admin/categories/create/_components/submit-data.tsx
+++ src/app/admin/categories/create/_components/submit-data.tsx
@@ -9,21 +9,27 @@ import toast from "react-hot-toast";
 import LoaderLayout from "@/components/loader-layout";
 import { createCategory } from "@/lib/RTK/slices/categories-slice";
 import { useAppDispatch } from "@/hooks/redux";
+import useCategories from "@/hooks/use-categories";
 export default function SubmitData() {
   const router = useRouter();
   const dispatch = useAppDispatch();
+  const { data } = useCategories();
+
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [subCateValues, setSubCateValues] = useState<
     { image: any; name: string }[]
   >([{ name: "", image: "" }]);
+
   const [mainCateValues, setMainCateValues] = useState<{
     image: any;
     name: string;
   }>({ name: "", image: "" });
+
   const trimSubCateVlues = subCateValues.map((el) => ({
     ...el,
     name: el.name.trim(),
   }));
+
   const trimMainCateVlues = {
     ...mainCateValues,
     name: mainCateValues.name.trim(),
@@ -35,7 +41,23 @@ export default function SubmitData() {
     (el) => Object.values(el).every(Boolean) && cateIsValid(el?.name)
   );
 
+  const filterRepeatedSubCateValues = filterSubCate.filter(
+    (obj, index, arr) =>
+      arr.findIndex((item) => item.name === obj.name) === index
+  );
+
+  console.log(filterRepeatedSubCateValues);
+
+  const findSameMainCate = data?.find(
+    (el) => el?.main_category?.name === mainCateValues.name
+  );
   const onSubmit = async () => {
+    if (findSameMainCate)
+      return toast.error("Main category label already exists in the database.");
+
+    if (findSameMainCate)
+      return toast.error("Main category label already exists in the database.");
+
     if (!Object.values(trimMainCateVlues).every(Boolean))
       return toast.error(
         "Please complete the incomplete fields in main category section."
@@ -43,7 +65,7 @@ export default function SubmitData() {
 
     if (!cateIsValid(trimMainCateVlues?.name))
       return toast.error(
-        " Main category name must be 2-50 characters long, containing only English letters."
+        "Main category name must be 2-50 characters long, containing only English letters."
       );
 
     if (filterSubCate.length === 0)
@@ -51,14 +73,18 @@ export default function SubmitData() {
         "At least one sub category must be added and completed."
       );
     setIsSubmitting(true);
-    await dispatch(
+    const create = await dispatch(
       createCategory({
         main_category: trimMainCateVlues,
         sub_categories: filterSubCate,
       })
     );
-    setIsSubmitting(false);
-    router.replace("/admin/categories");
+    console.log(create);
+
+    if (create.meta?.requestStatus === "fulfilled") {
+      setIsSubmitting(false);
+      router.replace("/admin/categories");
+    }
   };
 
   return (
