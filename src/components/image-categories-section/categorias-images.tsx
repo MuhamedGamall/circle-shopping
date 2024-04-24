b2ac172540a04e8a4a1f40a980538011ba501ba0
JimmyDeveloperMan <jimmy.moftah@gmail.com>
2024-04-24 16:35:53 +0200

    update category images slider

--- src/components/image-categories-section/categorias-images.tsx
+++ src/components/image-categories-section/categorias-images.tsx
@@ -3,14 +3,35 @@
 import CategoryImg from "./categories-img";
 import { SliderWrapper } from "../wrappers/slider-wrapper";
 import useCategories from "@/hooks/use-categories";
+import { Skeleton } from "../ui/skeleton";
+import { CarouselItem } from "../ui/carousel";
+import { cn } from "@/lib/utils";
+const LoadingSkeleton = () => {
+  const loadingArray = Array.from({ length: 22 });
+
+  return (
+
+        loadingArray.map((_, i) => (
+        <CarouselItem
+          key={i}
+          className={cn("w-full mr-3 h-full basis-1/5 sm:basis-1/12")}
+        >
+          <Skeleton className="h-[80px] w-[80px] rounded-full " />
+        </CarouselItem>
+      ))
+
+  );
+};
 export default function CategoriesImages() {
-const{data} =useCategories()
+  const { data, loading } = useCategories();
   return (
     <div className="bg-white py-4 px-2">
-      <SliderWrapper>
-        {data?.map((el,i) => (
-          <CategoryImg key={i} {...el}  />
-        ))}
+      <SliderWrapper className=" ">
+        {loading ? (
+          <LoadingSkeleton />
+        ) : (
+          data?.map((el, i) => <CategoryImg key={i} {...el} />)
+        )}
       </SliderWrapper>
     </div>
   );
--- src/components/image-categories-section/categories-img.tsx
+++ src/components/image-categories-section/categories-img.tsx
@@ -6,8 +6,8 @@ import { Category } from "@/types";
 
 export default function CategoryImg({ main_category }: Category) {
   return (
-    <CarouselItem className={cn("w-full h-full basis-1/5 sm:basis-1/12")}>
-      <Link href={"href"} className="">
+    <CarouselItem className={cn("w-full h-full basis-1/5 lg:basis-1/12 sm:basis-[15%] md:basis-[12%]")}>
+      <Link href={"href"} className="flex flex-col items-center  gap-3">
         <Image
           width={100}
           height={100}
@@ -16,6 +16,7 @@ export default function CategoryImg({ main_category }: Category) {
           alt=""
           className="w-full h-full object-cover rounded-full border-[#d6d6d6] block border"
         />
+        <p className=" text-center font-bold leading-3 lg:leading-5 capitalize lg:text-sm text-[12px] ">{main_category?.name}</p>
       </Link>
     </CarouselItem>
   );
