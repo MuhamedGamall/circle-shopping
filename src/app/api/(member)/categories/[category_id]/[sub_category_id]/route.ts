3bf1c8b2c87edd6d439c4d3c75518d753ff33856
JimmyDeveloperMan <jimmy.moftah@gmail.com>
2024-05-02 22:29:26 +0200

    update api routes

--- src/app/api/(member)/categories/[category_id]/[sub_category_id]/route.ts
+++ src/app/api/(member)/categories/[category_id]/[sub_category_id]/route.ts
@@ -8,56 +8,46 @@ import mongoConnect from "@/utils/mongo-connect";
 export async function GET(
   req: NextRequest,
   {
-    params: { category_id },
+    params: { category_id, sub_category_id },
   }: {
-    params: { category_id: string };
+    params: { category_id: string; sub_category_id: string };
   }
 ) {
   try {
     await mongoConnect();
-    const limit = parseInt(req.nextUrl.searchParams.get("limit") || "0");
 
     const filterCategories = {
       "main_category.name": category_id,
+      sub_categories: {
+        $elemMatch: {
+          name: sub_category_id,
+        },
+      },
     };
 
     const findCategory = await Category.findOne(filterCategories);
-    const bestSellerThreshold = 100;
+
     if (!findCategory) {
       return new NextResponse("Not Found", { status: 404 });
     }
 
+    const bestSellerThreshold = 100;
     const filterProducts = {
       "category.main_category": category_id,
+      "category.sub_category": sub_category_id,
       is_published: true,
     };
 
-    let products = await Product.find({
-      ...filterProducts,
-      "price.offer.discount_percentage": { $gte: 0.01 },
-    })
-      .limit(limit)
-      .lean();
+    let products = await Product.find(filterProducts).lean()
 
-    const alternativeData = await Product.find(filterProducts)
-      .limit(limit)
-      .lean();
-
-    const updateData = products.map((el, i) => ({
+    products = products.map((el, i) => ({
       ...el,
-      is_bestseller: el?.sales_count >= bestSellerThreshold,
+      is_bestseller: el.sales_count >= bestSellerThreshold,
     }));
-
-    products = products.length
-      ? updateData
-      : alternativeData.map((el, i) => ({
-          ...el,
-          is_bestseller: el?.sales_count >= bestSellerThreshold,
-        }));
 
     return NextResponse.json(products);
   } catch (error) {
-    console.log("[MEMBER:CATEGORY>DEALS]", error);
+    console.log("[MEMBER:CATEGORY>SUB_CATEGROY]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
